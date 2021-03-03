410c2b4edd182c0d55ed81b2b7a3a6dc7aaebfbb
Reo Yamanaka <ryamanaka807@gmail.com>
2021-03-02 23:05:26 -0500

    completed smallest to greatest sorting

--- script.py
+++ script.py
@@ -2,4 +2,39 @@ from random import shuffle
 initialList = [x for x in range(10)]
 shuffle(initialList)
 
-print(initialList)
+print("Initial randomized list: %s"%initialList)
+output = []
+
+def getMin(inputList):
+    currentMin = inputList[0]
+    for i in range(len(inputList)-1):
+        if inputList[i + 1] < currentMin:
+            currentMin = inputList[i + 1]
+    return currentMin
+
+def getMax(inputList):
+    currentMax = inputList[0]
+    for i in range(len(inputList) - 1):
+        if inputList[i + 1] > currentMax:
+            currentMax = inputList[i + 1]
+    return currentMax
+
+
+currentMin = getMin(initialList)
+currentMax = getMax(initialList)
+counter = 0
+
+while counter < len(initialList):
+    for i in initialList:
+        if i == currentMin:
+            output.append(i)
+            counter += 1
+    currentMin += 1
+
+print("The sorted list, from smallest to greatest: %s"%output)
+
+            
+        
+
+        
+
