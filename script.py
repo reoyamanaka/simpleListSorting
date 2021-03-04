4ac0dd06b9d6f90bee5e0a4540d0e7329eeef836
Reo Yamanaka <ryamanaka807@gmail.com>
2021-03-04 00:24:38 -0500

    added input validation

--- script.py
+++ script.py
@@ -19,30 +19,36 @@ def getMax(inputList):
             currentMax = inputList[i + 1]
     return currentMax
 
+while True:
+    print("How do you want to sort the randomized list?")
+    print("1. Increasing")
+    print("2. Decreasing")
+    sortingOption = input()
+    currentMin = getMin(initialList)
+    currentMax = getMax(initialList)
+    counter = 0
+    if sortingOption == "1":
+        while counter < len(initialList):
+            for i in initialList:
+                if i == currentMin:
+                    output.append(i)
+                    counter += 1
+            currentMin += 1
+        print("The sorted list, from smallest to greatest: %s"%output)
+        break
+    elif sortingOption == "2":
+        while counter < len(initialList):
+            for i in initialList:
+                if i == currentMax:
+                    output.append(i)
+                    counter += 1
+            currentMax -= 1
+        print("The sorted list, from greatest to smallest: %s"%output)
+        break
+    else:
+        print("\nInvalid entry. Choose either 1 or 2.\n")
 
-print("How do you want to sort the randomized list?")
-print("1. Increasing")
-print("2. Decreasing")
-sortingOption = input()
-currentMin = getMin(initialList)
-currentMax = getMax(initialList)
-counter = 0
-if sortingOption == "1":
-    while counter < len(initialList):
-        for i in initialList:
-            if i == currentMin:
-                output.append(i)
-                counter += 1
-        currentMin += 1
-    print("The sorted list, from smallest to greatest: %s"%output)
-elif sortingOption == "2":
-    while counter < len(initialList):
-        for i in initialList:
-            if i == currentMax:
-                output.append(i)
-                counter += 1
-        currentMax -= 1
-    print("The sorted list, from greatest to smallest: %s"%output)
+        
     
 
             
