8a300a395905ae03a61b902525491af6446584aa
Reo Yamanaka <ryamanaka807@gmail.com>
2021-04-12 19:57:54 -0400

    fixed and completed descending insertion sort

--- insertionSort.py
+++ insertionSort.py
@@ -8,12 +8,11 @@ def insertionSort(inputList, desc = False):
                 j -= 1
             inputList[j + 1] = anchor
     else:
-        for i in range(len(inputList) - 1, 0, -1):
+        for i in range(1, len(inputList)):
             anchor = inputList[i]
             j = i - 1
-            while j >= 0 and anchor < inputList[j]:
+            while j >= 0 and anchor > inputList[j]:
                 inputList[j + 1] = inputList[j]
                 j -= 1
             inputList[j + 1] = anchor
     return inputList
-
