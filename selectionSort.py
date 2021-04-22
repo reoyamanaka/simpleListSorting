885b801e947013b62dd54aaa4381236c402f278e
Reo Yamanaka <ryamanaka807@gmail.com>
2021-04-21 21:54:54 -0400

    added selection sort with descending order feature

--- selectionSort.py
+++ selectionSort.py
@@ -1,11 +1,20 @@
-from helperFunctions import getMin
-
 def selectionSort(elements, reverse = False):
     if not reverse:
         for i in range(len(elements) - 1):
             minIndex = i
             for j in range(minIndex + 1, len(elements)):
                 if elements[j] < elements[minIndex]:
+                    minIndex = j
+            if i != minIndex:
+                temp = elements[i]
+                elements[i] = elements[minIndex]
+                elements[minIndex] = temp
+
+    else:
+        for i in range(len(elements) - 1):
+            minIndex = i
+            for j in range(minIndex + 1, len(elements)):
+                if elements[j] > elements[minIndex]:
                     minIndex = j
             if i != minIndex:
                 temp = elements[i]
@@ -21,3 +30,7 @@ shuffle(a)
 print(a)
 selectionSort(a)
 print(a)
+selectionSort(a, reverse = True)
+print(a)
+
+
